3b3cdfd1f1aa74fe3ce36fec19ca16a6ea44dc82
Fetyali7 <70493307+Fetyali7@users.noreply.github.com>
2022-10-30 18:12:28 -0400

    Add files via upload

---
+++ mobile/Potato Cloud Fest/Potato Cloud Fest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Potato Cloud Fest
+//
+//  Created by Ali Fetanat on 2022-10-30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
